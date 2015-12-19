9ef4d2ce9c9938e5f4350d2870c605e0ea6f6cfa
Blue <sbx@everblue.info>
2015-12-18 22:34:27 -0800

    Update to build on linux (debian, at least). Add formatting for incoming messages. Replace tabs with spaces (:P)

--- Makefile
+++ Makefile
@@ -1,16 +1,14 @@
 #Customisable stuff here
-LINUX32_COMPILER = i686-pc-linux-gnu-gcc
-LINUX64_COMPILER = x86_64-pc-linux-gnu-gcc
+LINUX_COMPILER = gcc
 WIN32_COMPILER = /usr/bin/i586-mingw32-gcc
 WIN32_WINDRES = i586-mingw32-windres
-#LINUX_ARM_COMPILER = arm-pc-linux-gnu-gcc
 LINUX_ARM_COMPILER = arm-none-linux-gnueabi-gcc
 LINUX_PPC_COMPILER = powerpc-unknown-linux-gnu-gcc
 FREEBSD60_COMPILER = i686-pc-freebsd6.0-gcc
 MACPORT_COMPILER = i686-apple-darwin9-gcc-4.0.1
 
 LIBPURPLE_CFLAGS = -I/usr/include/libpurple -I/usr/local/include/libpurple 
-GLIB_CFLAGS = -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/lib/gtk-2.0/include/ -I/usr/include -I/usr/local/include/glib-2.0 -I/usr/local/lib/glib-2.0/include -I/usr/local/include
+GLIB_CFLAGS = -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/lib/x86_64-linux-gnu/gtk-2.0/include/ -I/usr/include -I/usr/local/include/glib-2.0 -I/usr/local/lib/glib-2.0/include -I/usr/local/include
 WIN32_DEV_DIR = /root/pidgin/win32-dev
 WIN32_PIDGIN_DIR = /root/pidgin/pidgin-2.3.0_win32
 WIN32_CFLAGS = -I${WIN32_DEV_DIR}/gtk_2_0/include/glib-2.0 -I${WIN32_PIDGIN_DIR}/libpurple/win32 -I${WIN32_PIDGIN_DIR}/pidgin/win32 -I${WIN32_DEV_DIR}/gtk_2_0/include -I${WIN32_DEV_DIR}/gtk_2_0/include/glib-2.0 -I${WIN32_DEV_DIR}/gtk_2_0/lib/glib-2.0/include -I${WIN32_DEV_DIR}/gtk_2_0/lib/gtk-2.0/include -Wno-format
@@ -33,7 +31,7 @@ clean:
 	rm -f irc_format.dll irc_format.so
 
 irc_format.so:	${SOURCES}
-	${LINUX32_COMPILER} ${LIBPURPLE_CFLAGS} -Wall ${GLIB_CFLAGS} -I. -g -O2 -pipe ${SOURCES} -o $@ -shared -fPIC -DPIC
+	${LINUX_COMPILER} ${LIBPURPLE_CFLAGS} -Wall ${GLIB_CFLAGS} -I. -g -O2 -pipe ${SOURCES} -o $@ -shared -fPIC -DPIC
 
 irc_format.dll:	${SOURCES}
 	${WIN32_COMPILER} ${LIBPURPLE_CFLAGS} -Wall -I. -g -O0 -pipe ${SOURCES} -o $@ -shared -mno-cygwin ${WIN32_CFLAGS} ${WIN32_LIBS}
--- pidgin-irc-format.c
+++ pidgin-irc-format.c
@@ -10,68 +10,101 @@
 #	define _(a) a
 #endif
 
-/* TODO use this to find the closest matching irc colour */
-static guchar
-ircformat_rgb_get_hue(guchar red, guchar green, guchar blue)
+static void
+ircformat_format_message_out(PurpleAccount *account, gchar **message)
 {
-	guchar min, max;
-	
-	min = MIN(red, MIN(green, blue));
-	max = MAX(red, MAX(green, blue));
-	
-	if (red == max)
-		return 43 * (green - blue) / (max - min);
-	if (green == max)
-		return 85 + 43 * (blue - red) / (max - min);
-	return 171 + 43 * (red - green) / (max - min);
+  gchar *tmp;
+#define irc_format(a, b) \
+              tmp = purple_strreplace(*message, a, b); \
+              g_free(*message); \
+              *message = tmp;
+
+  irc_format("<b>", "\x02"); irc_format("</b>", "\x02");
+  irc_format("<u>", "\x1F"); irc_format("</u>", "\x1F");
+  irc_format("<i>", "\x1D"); irc_format("</i>", "\x1D");
+
+  irc_format("<font color=\"#FFFFFF\">", "\x03" "0"); /* white */
+  irc_format("<font color=\"#000000\">", "\x03" "1"); /* black */
+  irc_format("<font color=\"#070292\">", "\x03" "2"); /* blue */
+  irc_format("<font color=\"#007700\">", "\x03" "3"); /* dark green */
+  irc_format("<font color=\"#FF2F00\">", "\x03" "4"); /* red */
+  irc_format("<font color=\"#803046\">", "\x03" "5"); /* brown */
+  irc_format("<font color=\"#7A3180\">", "\x03" "6"); /* purple */
+  irc_format("<font color=\"#FF9057\">", "\x03" "7"); /* orange */
+  irc_format("<font color=\"#FFFF00\">", "\x03" "8"); /* yellow */
+  irc_format("<font color=\"#78FF00\">", "\x03" "9"); /* green */
+  irc_format("<font color=\"#178A6E\">", "\x03" "10"); /* teal */
+  irc_format("<font color=\"#3CE9FF\">", "\x03" "11"); /* cyan */
+  irc_format("<font color=\"#2320E2\">", "\x03" "12"); /* light blue */
+  irc_format("<font color=\"#E659EE\">", "\x03" "13"); /* pink */
+  irc_format("<font color=\"#777777\">", "\x03" "14"); /* grey */
+  irc_format("<font color=\"#BBBBBB\">", "\x03" "15"); /* light grey */
+
+  irc_format("</font>", "\x03,");
+
 }
 
 static void
-ircformat_format_message(PurpleAccount *account, gchar **message)
+ircformat_format_message_in(PurpleAccount *account, gchar **message)
 {
 	gchar *tmp;
-#define irc_format(a, b) \
-							tmp = purple_strreplace(*message, a, b); \
+  /* This function is basically a copy-paste of the above one, but with the arguments flipped in the macro. I suck at C, sorry. Please make a less dumb version of this. */
+#define irc_format_r(a, b) \
+							tmp = purple_strreplace(*message, b, a); \
 							g_free(*message); \
 							*message = tmp;
-	
-	irc_format("<b>", "\x02"); irc_format("</b>", "\x02");
-	irc_format("<u>", "\x1F"); irc_format("</u>", "\x1F");
-	irc_format("<i>", "\x16"); irc_format("</i>", "\x16");
-	
-	irc_format("<font color=\"#FFFFFF\">", "\x03" "0"); /* white */
-	irc_format("<font color=\"#000000\">", "\x03" "1"); /* black */
-	irc_format("<font color=\"#070292\">", "\x03" "2"); /* blue */
-	irc_format("<font color=\"#007700\">", "\x03" "3"); /* dark green */
-	irc_format("<font color=\"#FF2F00\">", "\x03" "4"); /* red */
-	irc_format("<font color=\"#803046\">", "\x03" "5"); /* brown */
-	irc_format("<font color=\"#7A3180\">", "\x03" "6"); /* purple */
-	irc_format("<font color=\"#FF9057\">", "\x03" "7"); /* orange */
-	irc_format("<font color=\"#FFFF00\">", "\x03" "8"); /* yellow */
-	irc_format("<font color=\"#78FF00\">", "\x03" "9"); /* green */
-	irc_format("<font color=\"#178A6E\">", "\x03" "10"); /* teal */
-	irc_format("<font color=\"#3CE9FF\">", "\x03" "11"); /* cyan */
-	irc_format("<font color=\"#2320E2\">", "\x03" "12"); /* light blue */
-	irc_format("<font color=\"#E659EE\">", "\x03" "13"); /* pink */
-	irc_format("<font color=\"#777777\">", "\x03" "14"); /* grey */
-	irc_format("<font color=\"#BBBBBB\">", "\x03" "15"); /* light grey */
-	
-	irc_format("</font>", "\x03,");
-	
+
+	irc_format_r("<b>", "\x02"); irc_format_r("</b>", "\x02");
+	irc_format_r("<u>", "\x1F"); irc_format_r("</u>", "\x1F");
+	irc_format_r("<i>", "\x1D"); irc_format_r("</i>", "\x1D");
+
+	irc_format_r("<font color=\"#FFFFFF\">", "\x03" "0"); /* white */
+	irc_format_r("<font color=\"#000000\">", "\x03" "1"); /* black */
+	irc_format_r("<font color=\"#070292\">", "\x03" "2"); /* blue */
+	irc_format_r("<font color=\"#007700\">", "\x03" "3"); /* dark green */
+	irc_format_r("<font color=\"#FF2F00\">", "\x03" "4"); /* red */
+	irc_format_r("<font color=\"#803046\">", "\x03" "5"); /* brown */
+	irc_format_r("<font color=\"#7A3180\">", "\x03" "6"); /* purple */
+	irc_format_r("<font color=\"#FF9057\">", "\x03" "7"); /* orange */
+	irc_format_r("<font color=\"#FFFF00\">", "\x03" "8"); /* yellow */
+	irc_format_r("<font color=\"#78FF00\">", "\x03" "9"); /* green */
+	irc_format_r("<font color=\"#178A6E\">", "\x03" "10"); /* teal */
+	irc_format_r("<font color=\"#3CE9FF\">", "\x03" "11"); /* cyan */
+	irc_format_r("<font color=\"#2320E2\">", "\x03" "12"); /* light blue */
+	irc_format_r("<font color=\"#E659EE\">", "\x03" "13"); /* pink */
+	irc_format_r("<font color=\"#777777\">", "\x03" "14"); /* grey */
+	irc_format_r("<font color=\"#BBBBBB\">", "\x03" "15"); /* light grey */
+
+	irc_format_r("</font>", "\x03,");
+
 }
 
 static gboolean
 ircformat_sending_im_cb(PurpleAccount *account, const gchar *receiver, gchar **message)
 {
-	ircformat_format_message(account, message);
+  ircformat_format_message_out(account, message);
+  return FALSE;
+}
+
+static gboolean
+ircformat_receiving_im_cb(PurpleAccount *account, const gchar *receiver, gchar **message)
+{
+	ircformat_format_message_in(account, message);
 	return FALSE;
 }
 
 static gboolean
 ircformat_sending_chat_cb(PurpleAccount *account, gchar **message, gint id)
 {
-	ircformat_format_message(account, message);
+	ircformat_format_message_out(account, message);
 	return FALSE;
+}
+
+static gboolean
+ircformat_receiving_chat_cb(PurpleAccount *account, const gchar *receiver, gchar **message)
+{
+  ircformat_format_message_in(account, message);
+  return FALSE;
 }
 
 static void
@@ -81,7 +114,7 @@ ircformat_enable_formatting_on_conversation(PurpleConversation *conv)
 		return;
 	if (!g_str_equal(conv->account->protocol_id, "prpl-irc"))
 		return;
-	
+
 	purple_conversation_set_features(conv, purple_conversation_get_features(conv)
 			| PURPLE_CONNECTION_HTML | PURPLE_CONNECTION_NO_URLDESC
 			| PURPLE_CONNECTION_NO_FONTSIZE | PURPLE_CONNECTION_NO_IMAGES);
@@ -94,7 +127,7 @@ ircformat_disable_formatting_on_conversation(PurpleConversation *conv)
 		return;
 	if (!g_str_equal(conv->account->protocol_id, "prpl-irc"))
 		return;
-	
+
 	purple_conversation_set_features(conv, purple_conversation_get_features(conv) & ~PURPLE_CONNECTION_HTML);
 }
 
@@ -106,7 +139,7 @@ ircformat_enable_formatting_on_connection(PurpleConnection *pc)
 	pc->flags |= PURPLE_CONNECTION_NO_URLDESC;
 	pc->flags |= PURPLE_CONNECTION_NO_FONTSIZE;
 	pc->flags |= PURPLE_CONNECTION_NO_IMAGES;
-	
+
 	purple_conversation_foreach(ircformat_enable_formatting_on_conversation);
 }
 
@@ -114,7 +147,7 @@ static void
 ircformat_disable_formatting_on_connection(PurpleConnection *pc)
 {
 	pc->flags &= ~PURPLE_CONNECTION_HTML;
-	
+
 	purple_conversation_foreach(ircformat_disable_formatting_on_conversation);
 }
 
@@ -123,18 +156,21 @@ plugin_load(PurplePlugin *plugin)
 {
 	void *handle;
 	GList *conns;
-	
+
 	handle = purple_conversations_get_handle();
-	
-	purple_signal_connect(handle, "sending-im-msg", plugin, PURPLE_CALLBACK(ircformat_sending_im_cb), NULL);
-	purple_signal_connect(handle, "sending-chat-msg", plugin, PURPLE_CALLBACK(ircformat_sending_chat_cb), NULL);
-	
+
+  purple_signal_connect(handle, "sending-im-msg", plugin, PURPLE_CALLBACK(ircformat_sending_im_cb), NULL);
+  purple_signal_connect(handle, "sending-chat-msg", plugin, PURPLE_CALLBACK(ircformat_sending_chat_cb), NULL);
+
+	purple_signal_connect(handle, "receiving-im-msg", plugin, PURPLE_CALLBACK(ircformat_receiving_im_cb), NULL);
+	purple_signal_connect(handle, "receiving-chat-msg", plugin, PURPLE_CALLBACK(ircformat_receiving_chat_cb), NULL);
+
 	conns = purple_connections_get_all();
 	for(; conns; conns = conns->next)
 	{
 		ircformat_enable_formatting_on_connection(conns->data);
 	}
-	
+
 	return TRUE;
 }
 
@@ -142,13 +178,13 @@ static gboolean
 plugin_unload(PurplePlugin *plugin)
 {
 	GList *conns;
-	
+
 	conns = purple_connections_get_all();
 	for(; conns; conns = conns->next)
 	{
 		ircformat_disable_formatting_on_connection(conns->data);
 	}
-	
+
 	return TRUE;
 }
 
@@ -157,7 +193,7 @@ plugin_init(PurplePlugin *plugin)
 {
 }
 
-static PurplePluginInfo info = 
+static PurplePluginInfo info =
 {
 	PURPLE_PLUGIN_MAGIC,
 	2,
@@ -175,16 +211,16 @@ static PurplePluginInfo info =
 	"Allows you to use the Pidgin formatting toolbar to make messages bold and stuff",
 	"Eion Robb <eionrobb@gmail.com>",
 	"http://pidgin-irc-format.googlecode.com/", //URL
-	
+
 	plugin_load,
 	plugin_unload,
 	NULL,
-	
+
 	NULL,
 	NULL,
 	NULL,
 	NULL,
-	
+
 	NULL,
 	NULL,
 	NULL,
